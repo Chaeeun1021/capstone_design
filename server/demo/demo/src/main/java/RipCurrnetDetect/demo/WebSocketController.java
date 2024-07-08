404369f442741c7d1a213be239e67dd4f0c951c1
unh6unh6 <ruddbs3308@naver.com>
2024-07-08 15:09:48 +0900

    feat: test api 추가

--- server/demo/demo/src/main/java/RipCurrnetDetect/demo/WebSocketController.java
+++ server/demo/demo/src/main/java/RipCurrnetDetect/demo/WebSocketController.java
@@ -1,12 +1,16 @@
 package RipCurrnetDetect.demo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Controller
 public class WebSocketController {
     @PostMapping("/")
-    public void test(@RequestBody Object testObject) {
+    public ResponseEntity<Void> test(@RequestBody Object testObject) {
         System.out.println(testObject);
+        return ResponseEntity.ok().build();
     }
 }
