7ae7487ec82b10c565b9c63f1f527eba80675dea
unh6unh6 <ruddbs3308@naver.com>
2024-07-23 03:00:14 +0900

    feat: 영상 위 overlay 테스트

--- client/rip-current/src/VideoPlayer.js
+++ client/rip-current/src/VideoPlayer.js
@@ -1,20 +1,39 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
 import 'videojs-contrib-hls';
+import './VideoPlayer.css'; // CSS 파일을 임포트
 
 const VideoPlayer = ({ src }) => {
   const videoRef = useRef(null);
   const playerRef = useRef(null);
+  const [overlayData, setOverlayData] = useState(null);
 
   useEffect(() => {
+    const mockData = {
+      x: 100,
+      y: 400,
+      width: 70,
+      height: 100
+    };
+    setOverlayData(mockData);
+
+    const showOverlay = () => {
+      setOverlayData(mockData);
+      setTimeout(() => {
+        setOverlayData(null);
+      }, 5000); // Remove overlay after 5 seconds
+    };
+
+    showOverlay();
+
     const initPlayer = () => {
       if (videoRef.current) {
         const options = {
           autoplay: true,
           controls: true,
           sources: [{
-            src: src,
+            src: `${src}?timestamp=${new Date().getTime()}`, // 타임스탬프 추가
             type: 'application/x-mpegURL' // HLS 스트리밍 타입
           }]
         };
@@ -46,8 +65,21 @@ const VideoPlayer = ({ src }) => {
   }, [src]);
 
   return (
-    <div data-vjs-player>
+    <div className="video-container">
       <video ref={videoRef} className="video-js vjs-default-skin vjs-big-play-centered" playsInline></video>
+      {overlayData && (
+        <div className="overlay">
+          <div
+            className="overlay-box"
+            style={{
+              top: `${overlayData.y}px`,
+              left: `${overlayData.x}px`,
+              width: `${overlayData.width}px`,
+              height: `${overlayData.height}px`,
+            }}
+          ></div>
+        </div>
+      )}
     </div>
   );
 };
