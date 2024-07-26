9563d95ac08af35fff98007d036fca2d591dc11a
Chaeeun1021 <dmslv1021@naver.com>
2024-07-26 16:28:03 +0900

    이안류DTO 형식 변경

--- server/demo/demo/src/main/java/RipCurrnetDetect/demo/service/RipService.java
+++ server/demo/demo/src/main/java/RipCurrnetDetect/demo/service/RipService.java
@@ -16,20 +16,19 @@ public class RipService {
     private final RipRepository ripRepository;
     // 이안류 데이터 저장
     public RipCurrent saveRip(RipCurrentDTO ripCurrentDTO) throws JsonProcessingException {
-       // dto -> entity 변환
-        RipCurrent ripCurrent = new RipCurrent();
-        ripCurrent.setDateTime(ripCurrentDTO.getDateTime());
-        ripCurrent.setBoundingCount(ripCurrentDTO.getBoundingCount());
-        // List<Integer[]> 타입의 데이터를 DB에 저장하기 어려우므로 String으로 변환하여 저장
+        RipCurrent ripCurrent = null;
         List<List<Integer[]>> drawingDatas = ripCurrentDTO.getDrawing();
+        // dto -> entity 변환
         for (List<Integer[]> drawing : drawingDatas) {
-            ripCurrent.setDrawing(DataConvertor.listToString(ripCurrentDTO.getDrawing()));
-            ripCurrent = convertToEntity(ripCurrentDTO);
+            ripCurrent = new RipCurrent();
+            ripCurrent.setDateTime(ripCurrentDTO.getDateTime());
+            ripCurrent.setBoundingCount(ripCurrentDTO.getBoundingCount());
+            System.out.println(DataConvertor.listToString(drawing));
+            // List<Integer[]> 타입의 데이터를 DB에 저장하기 어려우므로 String으로 변환
+            ripCurrent.setDrawing(DataConvertor.listToString(drawing));
+            // db 저장
+            ripRepository.save(ripCurrent);
         }
-        // db 저장
-        ripRepository.save(ripCurrent);
-
-
         return ripCurrent;
     }
 
@@ -38,13 +37,4 @@ public class RipService {
         return ripRepository.findAll();
     }
 
-    // ripCurrentDTO(DTO) -> ripCurrent(Entity) 변환
-    private RipCurrent convertToEntity(RipCurrentDTO ripCurrentDTO) throws JsonProcessingException {
-        RipCurrent ripCurrent = new RipCurrent();
-        ripCurrent.setDateTime(ripCurrentDTO.getDateTime());
-        ripCurrent.setBoundingCount(ripCurrentDTO.getBoundingCount());
-        // List<Integer[]> 타입의 데이터를 DB에 저장하기 어려우므로 String으로 변환하여 저장
-        ripCurrent.setDrawing(DataConvertor.listToString(ripCurrentDTO.getDrawing()));
-        return ripCurrent;
-    }
 }
